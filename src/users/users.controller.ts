efce1c6b6a6035bceb13b8592c46f575103452e6
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-05-28 23:40:23 +0300

    adds auth guard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,6 +13,7 @@ import { IUserService } from './user.service.interface';
 const jwt = require('jsonwebtoken');
 import 'reflect-metadata';
 import { IConfigService } from '../config/config.service.interface';
+import { AuthGuard } from '../common/auth.guard';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -39,7 +40,7 @@ export class UserController extends BaseController implements IUserController {
 				path: '/info',
 				method: 'get',
 				func: this.info,
-				middlewares: [],
+				middlewares: [new AuthGuard()],
 			},
 		]);
 	}
@@ -70,11 +71,8 @@ export class UserController extends BaseController implements IUserController {
 	}
 
 	async info({ user }: Request, res: Response, next: NextFunction): Promise<void> {
-		// const result = await this.userService.createUser(body);
-		// if (!result) {
-		// 	return next(new HTTPError(422, 'Такой пользователь существует!'));
-		// }
-		this.ok(res, { email: user });
+		const userInfo = await this.userService.getUserInfo(user);
+		this.ok(res, { email: userInfo?.email, id: userInfo?.id });
 	}
 
 	private signJWT(email: string, secret: string): Promise<string> {
